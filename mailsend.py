e3340ba98571db1343fc40a2453608e3d4e59596
orkhanseyfullayev <112703511+orkhanseyfullayev@users.noreply.github.com>
2025-01-29 04:12:49 +0300

    mailsend.py

--- mailsend.py
+++ mailsend.py
@@ -5,47 +5,34 @@ from email.mime.image import MIMEImage
 import datetime
 
 def mail_send(image_path):
-    # Alarm zamanını al
     now = datetime.datetime.now()
-    formatted_time = now.strftime("%Y-%m-%d  %H:%M:%S")  # Yıl-Ay-Gün Saat:Dakika:Saniye
+    formatted_time = now.strftime("%Y-%m-%d  %H:%M:%S")
 
     subject = "ALARM!"
-    message = f"Kamerada haraket tespit edildi!<br><br><img src='cid:image1'><br>Alarm Zamanı: {formatted_time}"
+    message = f"Movement detected on camera!<br><br><img src='cid:image1'><br>Alarm Time: {formatted_time}"
 
-    # SMTP sunucusu ve gönderici detayları
-    myMailAdress = "muhtas2kouorkhan@gmail.com"
-    password = "rqnh adnq dmtc ddui"
-    sendTo = "orkhan.seyfullayev@gmail.com"
+    sender_email = "your_email@example.com" # Your email address
+    password = "your_email_password" # Your email password
+    send_to = "recipient@example.com" # Recipient email address
 
-    # MIMEMultipart nesnesi oluştur
-    msg = MIMEMultipart('related')  # 'related' alt-partları birbirine bağlamak için kullanılır
+    msg = MIMEMultipart('related')
     msg['Subject'] = subject
-    msg['From'] = myMailAdress
-    msg['To'] = sendTo
+    msg['From'] = sender_email
+    msg['To'] = send_to
 
-    # E-posta metin içeriğini HTML olarak ekle
     msg.attach(MIMEText(message, 'html'))
 
-    # Görüntüyü HTML içinde gömülü olarak ekle
     with open(image_path, 'rb') as file:
         img = MIMEImage(file.read())
-        img.add_header('Content-ID', '<image1>')  # HTML içinde referans vermek için Content-ID kullan
-        msg.attach(img)
+        img.add_header('Content-ID', '<image1>')
 
-    # SMTP sunucusu üzerinden giriş yap ve e-postayı gönder
     mail = SMTP("smtp.gmail.com", 587)
     mail.ehlo()
     mail.starttls()
-    mail.login(myMailAdress, password)
+    mail.login(sender_email, password)
     mail.send_message(msg)
     mail.quit()
     print("Mail Sent!")
 
 if __name__ == "__main__":
-    mail_send('current_frame.jpg')  # Görüntü dosyasının yolu
-
-
-
-
-# password'u: Google App Passwords bölümündenelde ettim. Linki:
-# https://myaccount.google.com/u/3/apppasswords?rapt=AEjHL4OXzU-0hh3ZgDVsYP4DeHKE38JMQEbdb-53rZr-NVAm3lgwORe-1bsrnVvz373EBm776NpSHDm9ps8Cpo_A3EG6dgyxksgJyRuUjcICn9exZXCuLqg
+    mail_send('current_frame.jpg')
